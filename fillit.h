5bfca351538dd9c7ddc499e547736c346c2c8fc9
fdaryn-h <syndima43@yandex.ru>
2019-02-04 16:53:34 +0300

    Initial commit, basic validation worked

---
+++ fillit.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldonnis <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/29 16:49:17 by ldonnis           #+#    #+#             */
+/*   Updated: 2019/02/04 12:33:11 by fdaryn-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+
+#endif
