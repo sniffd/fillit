a7c4193d5f8362b405966a9bd75c69f1a08053be
fdaryn-h <syndima43@yandex.ru>
2019-02-06 02:26:25 +0300

    added structure for tetrominos

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: ldonnis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 16:49:17 by ldonnis           #+#    #+#             */
-/*   Updated: 2019/02/04 12:33:11 by fdaryn-h         ###   ########.fr       */
+/*   Updated: 2019/02/06 02:18:08 by fdaryn-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,15 @@
 # include <unistd.h>
 # include "libft/libft.h"
 
+typedef struct	s_tr
+{
+	char		name;
+	int			h;
+	int			w;
+	int			o;
+	struct s_tr	*next;
+	struct s_tr	*prev;
+}				t_tr;
 
+int		validate(char *file);
 #endif
