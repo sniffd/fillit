0936db8f0cff437745cb3aa6797afd34399df37a
fdaryn-h <syndima43@yandex.ru>
2019-02-09 06:52:36 +0300

    norme fix

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: ldonnis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 16:49:17 by ldonnis           #+#    #+#             */
-/*   Updated: 2019/02/09 06:29:56 by fdaryn-h         ###   ########.fr       */
+/*   Updated: 2019/02/09 06:51:34 by fdaryn-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 typedef struct	s_tr
 {
 	char		name;
-	char 		l;
+	char		l;
 	int			h;
 	int			w;
 	int			o;
@@ -32,13 +32,13 @@ typedef struct	s_tr
 
 }				t_tr;
 
-int		validate(char *file);
-t_tr	*create_list(char *file);
-int		get_w(const char *str);
-int		get_h(const char *str);
-char	get_name_h(char *str, int *o);
-char	get_name_v(char *str, int *o);
-void	get_tetro(t_tr **list, t_tr **prev, char *buf, char *l);
-t_tr	*malloc_tetr(t_tr **prev);
-t_tr	*create_list(char *file);
+int				validate(char *file);
+t_tr			*create_list(char *file);
+int				get_w(const char *str);
+int				get_h(const char *str);
+char			get_name_h(char *str, int *o);
+char			get_name_v(char *str, int *o);
+void			get_tetro(t_tr **list, t_tr **prev, char *buf, char *l);
+t_tr			*malloc_tetr(t_tr **prev);
+t_tr			*create_list(char *file);
 #endif
